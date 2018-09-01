e5a04d0a48519a0eea7510c3fa273355d3d4fd56
Adam Clayman <adam@clayman.org>
2018-09-01 01:30:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,12 @@
 # code here!
 class School
+  attr_accessor :roster
+  
   def initialize(name)
     @name = name
   end
 
-  
+  def add_student(name, grade)
+    roster[grade] << name
+  end
 end
