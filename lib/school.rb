c641e15aa93f045c3f4798ed538a1a2a345ae1a8
Adam Clayman <adam@clayman.org>
2018-09-01 01:34:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,30 @@
 # code here!
 class School
-  roster = {}
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
 
   def add_student(name, grade)
-    if roster[grade] = nil
-      roster[grade] = []
+    if @roster[grade] = nil
+      @roster[grade] = []
     end
-    roster[grade] << name
+    @roster[grade] << name
   end
 
   def roster
-    roster
+    @roster
   end
 
   def grade(grade)
-    roster[grade]
+    @roster[grade]
   end
 
   def sort
-    roster.each do |grade, students|
+    @roster.each do |grade, students|
       students.sort!
     end
-    roster
+    @roster
   end
 end
